d3f52f8430881f287a8adc2f07f7c1f3f8afbc68
Craig Jolley <jolleycraig@gmail.com>
2018-11-29 14:55:44 -0500

    Tried to improve open government predictions; added some feature engineering routines along the way.

--- predict_open_gov.R
+++ predict_open_gov.R
@@ -1,10 +1,67 @@
 source('predict_systematic.R')
 
 ifs_basic <- load_all_ifs()
-open_gov_ifs <- ml_prep(open_gov,ifs_basic) 
+open_gov_ifs <- ml_prep(open_gov,ifs_basic) %>% na.omit
 
-xval(open_gov_ifs,lm_wrap) # 20.306; threw rank-deficiency error
-xval(open_gov_ifs,svm_wrap) # 0.399
-xval(open_gov_ifs,xgb_wrap) # 0.417
+xval(open_gov_ifs,lm_wrap,fold=100) # 32.01; threw rank-deficiency error
+xval(open_gov_ifs,svm_wrap,fold=100) # 0.424
+xval(open_gov_ifs,xgb_wrap,fold=100) # 0.452
 
 # This is the worst performance on any of the models; ideally I'd like to get this down below about 0.25.
+
+# Try selecting a smaller set of predictors with LASSO
+
+og_smaller <- open_gov_ifs %>% select(country,year,value,varstr,get_predictors(open_gov_ifs))
+xval(og_smaller,lm_wrap,fold=100) # 0.333 - no longer rank-deficient
+xval(og_smaller,svm_wrap,fold=100) # 0.441 
+xval(og_smaller,xgb_wrap,fold=100) # 0.401 
+
+more_predictors <- open_gov_ifs %>% 
+  add_geo %>% 
+  add_gender_disparities %>% 
+  get_predictors(cutoff='min')
+
+og_bigger <- open_gov_ifs %>% 
+  add_geo %>% 
+  add_gender_disparities %>% 
+  select(country,year,value,varstr,more_predictors)
+
+xval(og_bigger,lm_wrap,fold=100) # 0.306 - best so far
+xval(og_bigger,svm_wrap,fold=100) # 0.417
+xval(og_bigger,xgb_wrap,fold=100) # 0.379
+
+# I guess I shouldn't be shocked that with less data the simpler model with
+# fewer predictors wins!
+
+pred_scatter(og_bigger,lm_wrap)
+
+countries_wrong(og_smaller,lm_wrap,fold=100) %>% head(5)
+# So... what do Malaysia, Hungary, and Turkey have in common that leads to overestimates?
+# What do Nepal and South Korea have in common that leads to underestimates?
+# In almost all of the component measures, SK is near the top and Nepal near the bottom...
+countries_wrong(og_bigger,lm_wrap,fold=100) %>% head(5)
+# problems persist when I add more predictors, though avg deviations have gotten smaller
+
+with_derivs <- ifs_basic %>% 
+  add_gender_disparities %>% 
+  add_derivs %>%
+  add_geo
+
+open_gov_deriv <- ml_prep(open_gov,with_derivs) %>% na.omit 
+deriv_predictors_1se <- get_predictors(open_gov_deriv)
+deriv_predictors_min <- get_predictors(open_gov_deriv,cutoff='min')
+
+og_deriv_small <- open_gov_deriv %>% select(country,year,value,varstr,deriv_predictors_1se)
+xval(og_deriv_small,lm_wrap,fold=100) # 0.3299
+xval(og_deriv_small,svm_wrap,fold=100) # 0.3988
+xval(og_deriv_small,xgb_wrap,fold=100) # 0.39523
+
+og_deriv_big <- open_gov_deriv %>% select(country,year,value,varstr,deriv_predictors_min)
+xval(og_deriv_big,lm_wrap,fold=100) # 0.3553
+xval(og_deriv_big,svm_wrap,fold=100) # 0.3761
+xval(og_deriv_big,xgb_wrap,fold=100) # 0.3756
+
+# So adding in slope predictors doesn't do anything for my accuracy. I might be able
+# to do more with a different algorithm (KNN? Ridge regression?) or by better tuning
+# of the ones I've got. All of those things will require some work to enable better
+# parameterization with the wrapper constructs I'm using.
--- predict_systematic.R
+++ predict_systematic.R
@@ -2,6 +2,7 @@ source('utils.R')
 source('extract_indices.R')
 source('IFs_plots.R')
 library(ggrepel)
+library(glmnet)
 
 ###############################################################################
 # Load all IFs series. I should take a closer look at these to see if some
@@ -197,6 +198,11 @@ forecast_plot <- function(jsr,ifs,country_,wrap_fun) {
 # Model wrappers
 ###############################################################################
 lm_wrap <- function(train,test) {
+  if (!('value' %in% names(test))) { test$value <- NA }
+  is_const <- (apply(train,2,var,na.rm=TRUE) == 0)
+  is_const['value'] <- FALSE
+  train <- train[!is_const]
+  test <- test[!is_const]
   my_lm <- lm(value ~ .,data=train)
   predict(my_lm,test)
 }
@@ -233,13 +239,102 @@ xgb_wrap <- function(train,test,eta=0.3,max_depth=6,subsample=1,colsample_bytree
 }
 
 ###############################################################################
-# Feature engineering: try to improve continuity between history and forecast
+# Feature engineering: Get indicators for membership in geographic regions
 ###############################################################################
-features_end <- function(jsr,ifs) {
-  last_val <- jsr %>%
-    group_by(country) %>%
-    arrange(year) %>%
-    summarize(last_value=last(value))
-  left_join(ifs,last_val,by='country')
+add_geo <- function(d) {
+  d %>% mutate(
+    africa = ifelse(country %in% africa,1,0),
+    asia = ifelse(country %in% asia,1,0),
+    lac = ifelse(country %in% lac,1,0),
+    me = ifelse(country %in% me,1,0),
+    e_and_e = ifelse(country %in% e_and_e,1,0),
+    usaid_country = ifelse(country %in% usaid_countries,1,0),
+    high_income = ifelse(country %in% high_income,1,0),
+    umic = ifelse(country %in% umic,1,0),
+    lmic = ifelse(country %in% lmic,1,0),
+    low_income = ifelse(country %in% low_income,1,0))
 }
 
+###############################################################################
+# Feature engineering: Convert gender-disaggregated variables into disparity measures.
+# Scaling such that they should be close to one if male and female rates are
+# equal, 0 if females are zero, 2 if males are zero.
+###############################################################################
+add_gender_disparities <- function(d) {
+  eps <- 1e-6 # avoid dividing by zero
+  death_causes <- c('aids','cancer','diabetes','diarrhea','digestive','intinj',
+                    'malaria','mental','othcomm','othnoncomm','respinfec','respiratory','traffic','unintinj')
+  for (dc in death_causes) {
+    male_str <- paste0('deaths_male_',dc)
+    female_str <- paste0('deaths_female_',dc)
+    new_str <- paste0('gender_disparity_',dc)
+    d[,new_str] <- 2* d[,female_str]/(d[,female_str] + d[,male_str] + eps)
+  }
+  d$gender_disparity_smoking <- 2* d$smoking_female / (d$smoking_female + d$smoking_male + eps)
+  d
+}
+
+###############################################################################
+# Feature engineering: Add derivatives
+###############################################################################
+add_derivs <- function(d) {
+  out <- d 
+  ignore_vars <- c('country','varstr','value','year')
+  get_deriv <- function(d) {
+    # assumes columns are country, year, var
+    names(d) <- c('country','year','x')
+    countries <- d$country %>% unique
+    plyr::ldply(countries,function(i) {
+      di <- d %>% filter(country==i)
+      x_loess <- loess(x ~ year,data=di)
+      t_new <- min(di$year):max(di$year)
+      pred <- predict(x_loess,t_new)
+      dx_dt <- diff(pred) / diff(t_new)
+      data.frame(country=i,year=t_new,slope=c(dx_dt,dx_dt[length(dx_dt)]))
+    })
+  }
+  for (n in setdiff(names(d),ignore_vars)) {
+    print(n)
+    tmp_n <- d %>% select(country,year,n) 
+    gd <- get_deriv(tmp_n)
+    names(gd) <- c('country','year',paste0('slope_',n))
+    out <- left_join(out,gd,by=c('country','year')) 
+  }
+  out
+}
+
+###############################################################################
+# Feature selection: Get a reduced set of features using LASSO
+###############################################################################
+get_predictors <- function(d,show_plot=TRUE,cutoff='1se') {
+  x <- d %>% select(-country,-year,-value,-varstr) %>% as.matrix
+  y <- d$value
+  cv.out <- cv.glmnet(x,y,alpha=1,family='gaussian',type.measure = 'mse')
+  if (show_plot) { plot(cv.out) }
+  if (cutoff=='1se') { tmp <- coef(cv.out,s=cv.out$lambda.1se) }
+  else { tmp <- coef(cv.out,s=cv.out$lambda.min) }
+  colnames(x)[tmp@i]
+}
+
+###############################################################################
+# Handy function to spot systematic errors
+###############################################################################
+countries_wrong <- function(d,wrap_fun,filter_year=NULL,test_frac=0.3,fold=10) {
+  plyr::ldply(1:fold,function(i) {
+    n <- nrow(d)
+    split <- (runif(n) < test_frac)
+    train <- d[!split,]  %>% ml_impute %>% select(-country,-varstr)
+    tmp <- d[split,] %>% ml_impute 
+    test <- tmp %>% select(-country,-varstr)
+    pred <- wrap_fun(train,test)
+    tmp$ndev <- abs(pred - test$value)
+    tmp$dev <- pred - test$value
+    cutoff <- quantile(tmp$ndev,probs=0.95)
+    worst <- tmp %>% 
+      filter(ndev > cutoff) %>% 
+      select(country,dev)
+  }) %>%
+    group_by(country) %>%
+    summarize(count=n(),mean_dev=mean(dev)) %>%
+    arrange(desc(count))
+}

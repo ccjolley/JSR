e2662af63d87c70b6f675029323862d9c0598d72
Craig Jolley <jolleycraig@gmail.com>
2018-12-03 14:41:56 -0500

    Set things up for better hyperparameter tuning, added some more variables.

--- predict_open_gov.R
+++ predict_open_gov.R
@@ -1,22 +1,29 @@
 source('predict_systematic.R')
 
 ifs_basic <- load_all_ifs()
-open_gov_ifs <- ml_prep(open_gov,ifs_basic) %>% na.omit
+open_gov_ifs <- all_jsr %>% filter(varstr=='Open Government') %>%
+  ml_prep(ifs_basic) %>% na.omit
 
-xval(open_gov_ifs,lm_wrap,fold=100) # 32.01; threw rank-deficiency error
-xval(open_gov_ifs,svm_wrap,fold=100) # 0.424
-xval(open_gov_ifs,norm_svm_wrap,fold=100) # 0.4419; normalization doesn't help much
-xval(open_gov_ifs,xgb_wrap,fold=100) # 0.452
+xval(open_gov_ifs,lm_wrap,fold=100) # 22.57; threw rank-deficiency error
+xval(open_gov_ifs,svm_wrap,fold=100) # 0.461
+xval(open_gov_ifs,norm_svm_wrap,fold=100) # 0.445; normalization doesn't help much
+xval(open_gov_ifs,xgb_wrap,fold=100) # 0.4036
 
 # This is the worst performance on any of the models; ideally I'd like to get this down below about 0.25.
 
 # Try selecting a smaller set of predictors with LASSO
 
 og_smaller <- open_gov_ifs %>% select(country,year,value,varstr,get_predictors(open_gov_ifs))
-xval(og_smaller,lm_wrap,fold=100) # 0.333 - no longer rank-deficient
-xval(og_smaller,svm_wrap,fold=100) # 0.441 
-xval(og_smaller,norm_svm_wrap,fold=100) # 0.441
-xval(og_smaller,xgb_wrap,fold=100) # 0.401 
+names(og_smaller)
+xval(og_smaller,lm_wrap,fold=100) # 0.317 - no longer rank-deficient
+xval(og_smaller,svm_wrap,fold=100) # 0.354 
+xval(og_smaller,norm_svm_wrap,fold=100) # 0.372
+xval(og_smaller,xgb_wrap,fold=100) # 0.377
+# try getting number of rounds instead
+xval(og_smaller,xgb_wrap,fold=10,nrounds=NULL)
+xval(og_smaller,xgb_wrap,fold=100,nrounds=18) # 0.3612
+# with the number of rounds that low, I should probably slow down 
+# the learning rate
 
 more_predictors <- open_gov_ifs %>% 
   add_geo %>% 
@@ -27,10 +34,11 @@ og_bigger <- open_gov_ifs %>%
   add_geo %>% 
   add_gender_disparities %>% 
   select(country,year,value,varstr,more_predictors)
+names(og_bigger)
 
-xval(og_bigger,lm_wrap,fold=100) # 0.306 - best so far
-xval(og_bigger,svm_wrap,fold=100) # 0.417
-xval(og_bigger,xgb_wrap,fold=100) # 0.379
+xval(og_bigger,lm_wrap,fold=100) # 0.286 - best so far
+xval(og_bigger,svm_wrap,fold=100) # 0.400
+xval(og_bigger,xgb_wrap,fold=100) # 0.353
 
 # I guess I shouldn't be shocked that with less data the simpler model with
 # fewer predictors wins!
@@ -44,39 +52,12 @@ countries_wrong(og_smaller,lm_wrap,fold=100) %>% head(5)
 countries_wrong(og_bigger,lm_wrap,fold=100) %>% head(5)
 # problems persist when I add more predictors, though avg deviations have gotten smaller
 
-with_derivs <- ifs_basic %>% 
-  add_gender_disparities %>% 
-  add_derivs %>%
-  add_geo
-
-open_gov_deriv <- ml_prep(open_gov,with_derivs) %>% na.omit 
-deriv_predictors_1se <- get_predictors(open_gov_deriv)
-deriv_predictors_min <- get_predictors(open_gov_deriv,cutoff='min')
-
-og_deriv_small <- open_gov_deriv %>% select(country,year,value,varstr,deriv_predictors_1se)
-xval(og_deriv_small,lm_wrap,fold=100) # 0.3299
-xval(og_deriv_small,svm_wrap,fold=100) # 0.3988
-xval(og_deriv_small,xgb_wrap,fold=100) # 0.39523
-
-og_deriv_big <- open_gov_deriv %>% select(country,year,value,varstr,deriv_predictors_min)
-xval(og_deriv_big,lm_wrap,fold=100) # 0.3553
-xval(og_deriv_big,svm_wrap,fold=100) # 0.3761
-xval(og_deriv_big,xgb_wrap,fold=100) # 0.3756
-
-# So adding in slope predictors doesn't do anything for my accuracy. I might be able
-# to do more with a different algorithm (KNN? Ridge regression?) or by better tuning
-# of the ones I've got. All of those things will require some work to enable better
-# parameterization with the wrapper constructs I'm using.
-
 library(FNN)
-
-knn_tune <- plyr::ldply(1:25,function(k) knn_xval(og_smaller,k,fold=100))
+knn_tune <- plyr::ldply(1:25,function(k) knn_xval_old(og_smaller,k,fold=100))
 ggplot(knn_tune,aes(x=k,y=mean)) +
   geom_line(color='#BA0C2F') +
   geom_errorbar(aes(ymax=hi,ymin=lo),width=0.5) +
   theme_USAID + colors_USAID
-
-# Looks like my best bet is k=5, with NRMSE = 0.28166
 
 knn_tune2 <- plyr::ldply(1:25,function(k) knn_xval(og_bigger,k,fold=100))
 ggplot(knn_tune2,aes(x=k,y=mean)) +
@@ -84,6 +65,3 @@ ggplot(knn_tune2,aes(x=k,y=mean)) +
   geom_errorbar(aes(ymax=hi,ymin=lo),width=0.5) +
   theme_USAID + colors_USAID
 # Doesn't do as well as og_smaller does.
-
-# This is still my worst-performing model, but improving it further would require
-# hyperparameter tuning (for SVM, xgboost) and maybe also model stacking.
--- predict_systematic.R
+++ predict_systematic.R
@@ -4,12 +4,18 @@ source('IFs_plots.R')
 library(ggrepel)
 library(glmnet)
 
+load_ifs <- function() {
+  if(!file.exists('IFs_imputed.RData')) {
+    
+  }
+}
+
 ###############################################################################
 # Load all IFs series. I should take a closer look at these to see if some
 # might need further engineering, e.g., by normalizing by population or 
 # calculating gender differences. Put these in a different function, though.
 ###############################################################################
-load_all_ifs <- function() {
+load_raw_ifs <- function() {
   wd <- getwd()
   setwd('./IFs_exports/biz')
   biz_vars <- load_ifs(list.files(pattern='.*txt$')) 
@@ -29,8 +35,15 @@ load_all_ifs <- function() {
   ict_vars <- load_ifs(list.files(pattern='.*txt$'))
   setwd('../trade')
   trade_vars <- load_ifs(list.files(pattern='.*txt$'))
+  setwd('../diplo')
+  diplo_vars <- load_ifs(list.files(pattern='.*txt$'))
+  setwd('../state_failure')
+  state_fail_vars <- load_ifs(list.files(pattern='.*txt$'))
+  setwd('../poverty')
+  poverty_vars <- load_ifs(list.files(pattern='.*txt$'))
   setwd('..')
-  root_vars <- load_ifs(c('gini.txt','urban.txt','pop_density.txt','population.txt'))
+  root_vars <- load_ifs(c('gini.txt','urban.txt','pop_density.txt','population.txt',
+                          'gdppc_mer.txt','gdppc_ppp.txt'))
   setwd(wd)
   full_join(biz_vars,demo_vars,by=c('country','year')) %>%
     full_join(ed_vars,by=c('country','year')) %>%
@@ -40,6 +53,9 @@ load_all_ifs <- function() {
     full_join(health_vars,by=c('country','year')) %>%
     full_join(ict_vars,by=c('country','year')) %>%
     full_join(trade_vars,by=c('country','year')) %>%
+    full_join(diplo_vars,by=c('country','year')) %>%
+    full_join(state_fail_vars,by=c('country','year')) %>%
+    full_join(poverty_vars,by=c('country','year')) %>%
     full_join(root_vars,by=c('country','year'))
 }
 
@@ -60,16 +76,17 @@ ml_prep <- function(jsr,ifs) {
 ################################################################################
 # To fill in missing IFs values, call an impute function. For now just using
 # something simple (median); could try something more sophisticated later.
-#
-# NOTE: Always call this *after* the train-test split; otherwise we are data
-# snooping!
-###############################################################################
+################################################################################
 median_sub <- function(s) {
   s[is.na(s)] <- median(s[!is.na(s)])
   s
 }
 
-loess_sub <- function(df) {
+loess_sub <- function(df,extrapolation_window=10) {
+  if (length(intersect(c('country','year','x'),names(df))) != 3) {
+    message('ERROR in loess_sub, columns should be country, year, x')
+    return(NULL)
+  }
   all_years <- data.frame(year=min(df$year):max(df$year),country=df$country[1])
   df <- right_join(df,all_years,by=c('country','year'))
   if (sum(is.na(df$x)) == 0) { return(df) }
@@ -80,9 +97,12 @@ loess_sub <- function(df) {
     mutate(x=ifelse(is.na(x),xpred,x)) %>%
     select(-xpred)
   if (sum(is.na(j$x)) == 0) { return(j) }
-  # if there are still NAs, extrapolate using a linear function, but bound 
-  # extrapolation at the historical min and max values
-  lm_x <- lm(x ~ year,data=j)
+  # if there are still NAs, they will be at the beginning of the series
+  # use linear extrapolation, but bound extrapolation at the historical min 
+  # and max values, and use a finite extrapolation window rather than the
+  # entire series
+  extrap_df <- j %>% na.omit %>% arrange(year) %>% head(extrapolation_window)
+  lm_x <- lm(x ~ year,data=extrap_df)
   new_data <- data.frame(year = j$year[is.na(j$x)])
   new_data$xpred <- predict(lm_x,new_data)
   x_min <- min(j$x,na.rm=TRUE)
@@ -93,13 +113,13 @@ loess_sub <- function(df) {
   left_join(j,new_data,by='year') %>%
     mutate(x=ifelse(is.na(x),xpred,x)) %>%
     select(-xpred)
-  # TODO: Actually, I can leverage the fact that any remaning NAs at this last
-  # step will be at the beginning of the time series, since loess will fill in
-  # gaps and there is always data at the end (due to predictions). So just
-  # base the extrapolation on the first n years after data appear. For example,
-  # fdi_inflows in China look weird when I get this wrong. This will be a tunable
-  # parameter that I can use to adjust for optimum accuracy globally (across metrics).
 }
+
+# # test code for extrapolation
+# ifs_basic %>% filter(country=='Russia') %>% select(fdi_inflows,year,country) %>%
+#   rename(x=fdi_inflows) %>%
+#   loess_sub %>%
+#   ggplot(aes(x=year,y=x)) + geom_point() + geom_line()
 
 ml_impute <- function(d,impute_fn=median_sub) {
   res <- d %>% 
@@ -127,7 +147,6 @@ ml_impute_loess <- function(df,impute_fn=loess_sub) {
   }
   df
 }
-
 # TODO: do I still need ml_impute now that I have loess? Check to see what
 # it's actually doing in the places where it's being called. To me, it makes
 # sense (now) to impute just once on the IFs data at the beginning, rather
@@ -138,7 +157,7 @@ ml_impute_loess <- function(df,impute_fn=loess_sub) {
 ###############################################################################
 # Reusable cross-validation code
 ###############################################################################
-xval <- function(d,wrap_fun,fold=10,test_frac=0.1,lo=NULL,hi=NULL) {
+xval <- function(d,wrap_fun,fold=10,test_frac=0.1,lo=NULL,hi=NULL,...) {
   if (sum(!is.na(d$value)) < 200) { test_frac = 0.2}
   sapply(1:fold,function(i) {
     n <- nrow(d)
@@ -148,7 +167,7 @@ xval <- function(d,wrap_fun,fold=10,test_frac=0.1,lo=NULL,hi=NULL) {
     if (!is.null(lo) & !is.null(hi)) {
       train$value <- make_infinite(train$value,lo,hi)
     }
-    pred <- wrap_fun(train,test)
+    pred <- wrap_fun(train,test,...)
     if (!is.null(lo) & !is.null(hi)) {
       pred <- make_finite(pred,lo,hi)
     }
@@ -159,7 +178,7 @@ xval <- function(d,wrap_fun,fold=10,test_frac=0.1,lo=NULL,hi=NULL) {
 ###############################################################################
 # Scatterplot visualization
 ###############################################################################
-pred_scatter <- function(d,wrap_fun,filter_year=NULL,test_frac=0.3,lo=NULL,hi=NULL) {
+pred_scatter <- function(d,wrap_fun,filter_year=NULL,test_frac=0.3,lo=NULL,hi=NULL,...) {
   if (is.null(filter_year)) { filter_year <- max(d$year)}
   if (length(filter_year)==1) {
     title <- paste0(d$varstr[1],' (',filter_year,')')
@@ -175,7 +194,7 @@ pred_scatter <- function(d,wrap_fun,filter_year=NULL,test_frac=0.3,lo=NULL,hi=NU
   tmp <- d[split,] %>% ml_impute 
   test <- tmp %>% select(-country,-varstr)
   plotme <- tmp %>% select(country,year,value)
-  plotme$pred <- wrap_fun(train,test)
+  plotme$pred <- wrap_fun(train,test,...)
   if (!is.null(lo) & !is.null(hi)) {
     plotme$value <- make_finite(plotme$value,lo,hi)
     plotme$pred <- make_finite(plotme$pred,lo,hi)
@@ -203,7 +222,7 @@ country_trend <- function(d,country_,wrap_fun) {
   tmp <- d %>% ml_impute
   plotme <- tmp %>% select(country,year,value)
   fun_me <- tmp %>% select(-country,-varstr)
-  plotme$pred <- wrap_fun(fun_me,fun_me)
+  plotme$pred <- wrap_fun(fun_me,fun_me,...)
   plotme %>%
     filter(country==country_) %>%
     melt(id.vars=c('country','year')) %>%
@@ -227,7 +246,7 @@ jsr_forecast <- function(jsr,ifs,wrap_fun,lo=NULL,hi=NULL) {
     train$value <- make_infinite(train$value,lo,hi)
   }
   test$value <- wrap_fun(select(train,-country),
-                         select(test,-country))
+                         select(test,-country),...)
   if (!is.null(lo) & !is.null(hi)) {
     train$value <- make_finite(train$value,lo,hi)
     test$value <- make_finite(test$value,lo,hi)
@@ -252,7 +271,7 @@ forecast_plot <- function(jsr,ifs,country_,wrap_fun) {
 ###############################################################################
 # Model wrappers
 ###############################################################################
-lm_wrap <- function(train,test) {
+lm_wrap <- function(train,test,...) {
   if (!('value' %in% names(test))) { test$value <- NA }
   is_const <- (apply(train,2,var,na.rm=TRUE) == 0)
   is_const['value'] <- FALSE
@@ -262,16 +281,36 @@ lm_wrap <- function(train,test) {
   predict(my_lm,test)
 }
 
+library(FNN)
+knn_wrap <- function(train,test,...) {
+  # filter values that are nearly constant
+  is_const <- (apply(train,2,var,na.rm=TRUE) == 0)
+  is_const['value'] <- FALSE
+  train <- train[!is_const]
+  test <- test[!is_const]
+  # prepare for KNN
+  x <- train %>% select(-value)
+  xtest <- test %>% select(-value)
+  # scale test according to training data
+  for (n in names(xtest)) { 
+    xtest[[n]] <- xtest[[n]] - mean(x[[n]])
+    xtest[[n]] <- xtest[[n]] / sd(x[[n]])
+  }
+  x <- scale(x)
+  my_knn <- knn.reg(train=x,test=xtest,y=train$value,...)
+  my_knn$pred
+}
+
 library(e1071)
-svm_wrap <- function(train,test,eps=0.1,cost=64) {
+svm_wrap <- function(train,test,...) {
   is_const <- (apply(train,2,var,na.rm=TRUE) == 0)
   train <- train[!is_const]
   test <- test[!is_const]
-  my_svm <- svm(value ~ .,data=train,epsilon=eps,cost=cost)
+  my_svm <- svm(value ~ .,data=train,... = )
   predict(my_svm,test)
 }
 
-norm_svm_wrap <- function(train,test,eps=0.1,cost=64) {
+norm_svm_wrap <- function(train,test,...) {
   is_const <- (apply(train,2,var,na.rm=TRUE) == 0)
   train <- train[!is_const]
   test <- test[!is_const]
@@ -281,13 +320,12 @@ norm_svm_wrap <- function(train,test,eps=0.1,cost=64) {
     test[[n]] <- test[[n]] / sd(train[[n]])
     train[[n]] <- scale(train[[n]])
   }
-  my_svm <- svm(value ~ .,data=train,epsilon=eps,cost=cost)
+  my_svm <- svm(value ~ .,data=train,...)
   predict(my_svm,test)
 }
 
 library(xgboost)
-xgb_wrap <- function(train,test,eta=0.3,max_depth=6,subsample=1,colsample_bytree=0.8,
-                     verbose=0,nrounds=80) {
+xgb_wrap <- function(train,test,nrounds=100,...) {
   if (!('value' %in% names(test))) { test$value <- NA}
   dtrain <- xgb.DMatrix(data=select(train,-value) %>% as.matrix,
                         label=train$value)
@@ -296,13 +334,10 @@ xgb_wrap <- function(train,test,eta=0.3,max_depth=6,subsample=1,colsample_bytree
   if (is.null(nrounds)) { # using xval to determine nrounds
     watchlist <- list(train=dtrain, test=dtest)
     my_xgb <- xgb.train(data=dtrain,nrounds=200,watchlist=watchlist,  
-                        eta=eta,max_depth=max_depth,subsample=subsample,colsample_bytree=subsample,
-                        nthread=4,early_stopping_rounds=5,verbose=verbose)
+                        early_stopping_rounds=5,verbose=0,nthread=4,...)
     paste0(my_xgb$best_iteration,' iterations') %>% print
   } else {
-    my_xgb <- xgb.train(data=dtrain,nrounds=nrounds,  
-                        eta=eta,max_depth=max_depth,subsample=subsample,colsample_bytree=subsample,
-                        nthread=4,verbose=verbose)
+    my_xgb <- xgb.train(data=dtrain,nrounds=nrounds,nthread=4,...)
   }
   predict(my_xgb,dtest)
 }
@@ -313,23 +348,15 @@ xgb_wrap <- function(train,test,eta=0.3,max_depth=6,subsample=1,colsample_bytree
 # I'll also want some way to get a verson of xval() that gets me the sort of
 # ldply-friendly output I get here for hyperparameter tuning.
 ###############################################################################
-library(FNN)
 knn_xval <- function(d,k,fold=10,test_frac=0.1) {
+  message('k = ',k)
+  d <- d %>% select(-country,-varstr)
   nrmse <- sapply(1:fold,function(i) {
     split <- (runif(nrow(d)) < test_frac)
     train <- d[!split,]
     test <- d[split,]
-    x <- train %>% select(-country,-year,-value,-varstr)
-    xtest <- test %>% select(-country,-year,-value,-varstr) 
-    # scale test according to training data
-    for (n in names(xtest)) { 
-      xtest[[n]] <- xtest[[n]] - mean(x[[n]])
-      xtest[[n]] <- xtest[[n]] / sd(x[[n]])
-    }
-    x <- scale(x)
-    y <- train$value
-    knn_i <- knn.reg(train=x,test=xtest,y=train$value,k=k)
-    sqrt(mean((test$value-knn_i$pred)^2))/IQR(test$value)
+    pred <- knn_wrap(train,test,k=k)
+    sqrt(mean((test$value-pred)^2))/IQR(test$value)
   }) 
   ci <- t.test(nrmse)$conf.int
   data.frame(k=k,mean=mean(nrmse),lo=ci[1],hi=ci[2])

937f53f7cfc4869f0f98f6d3bc1a25c92e54220f
Craig Jolley <jolleycraig@gmail.com>
2019-01-23 15:58:56 -0500

    don't need NULL entries in hyperparameter lists anymore

--- compare_best.R
+++ compare_best.R
@@ -5,20 +5,20 @@ library(purrr)
 ifs_basic <- load_all_ifs()
 
 model_list <- list(
-  biodiv=list(label='Biodiversity & Habitat', wrapper=xgb_wrap, prepare=NULL),
-  biz_env=list(label='Business Environment', wrapper=xgb_wrap, prepare=NULL),
-  child_health=list(label='Child Health', wrapper=xgb_wrap, prepare=NULL),
-  diag_acc=list(label='Diagonal Accountability', wrapper=xgb_wrap, prepare=NULL),
-  export_div=list(label='Export Diversification', wrapper=xgb_wrap, prepare=NULL),
-  gender_gap=list(label='Economic gender gap', wrapper=xgb_wrap, prepare=NULL),
-  gov_effect=list(label='Government Effectiveness', wrapper=xgb_wrap, prepare=NULL),
-  group_eq=list(label='Group Equality', wrapper=xgb_wrap, prepare=NULL),
-  ict_use=list(label='ICT Use', wrapper=xgb_wrap, prepare=NULL),
-  lib_dem=list(label='Liberal Democracy', wrapper=xgb_wrap, prepare=NULL),
-  open_gov=list(label='Open Government', wrapper=xgb_wrap, prepare=NULL),
-  safety=list(label='Safety & Security', wrapper=xgb_wrap, prepare=NULL),
-  tax_admin=list(label='Tax Administration', wrapper=lm_wrap, prepare=lasso_1se),
-  trade_freedom=list(label='Trade Freedom', wrapper=xgb_wrap, prepare=NULL)
+  biodiv=list(label='Biodiversity & Habitat', wrapper=xgb_wrap),
+  biz_env=list(label='Business Environment', wrapper=xgb_wrap),
+  child_health=list(label='Child Health', wrapper=xgb_wrap),
+  diag_acc=list(label='Diagonal Accountability', wrapper=xgb_wrap),
+  export_div=list(label='Export Diversification', wrapper=xgb_wrap),
+  gender_gap=list(label='Economic gender gap', wrapper=xgb_wrap),
+  gov_effect=list(label='Government Effectiveness', wrapper=xgb_wrap),
+  group_eq=list(label='Group Equality', wrapper=xgb_wrap),
+  ict_use=list(label='ICT Use', wrapper=xgb_wrap),
+  lib_dem=list(label='Liberal Democracy', wrapper=xgb_wrap),
+  open_gov=list(label='Open Government', wrapper=xgb_wrap),
+  safety=list(label='Safety & Security', wrapper=xgb_wrap),
+  tax_admin=list(label='Tax Administration', wrapper=lm_wrap, feature=lasso_min),
+  trade_freedom=list(label='Trade Freedom', wrapper=xgb_wrap)
 )
 
 ###############################################################################
@@ -35,4 +35,13 @@ data.frame(model=map_chr(model_list,"label"),score=best_scores) %>%
   xlab(NULL) + ylab('NRMSE') +
   ggtitle('Scores as of 01/11/2019')
 
-
+plotme <- data.frame(score=best_scores,
+                     data=map_int(model_list, function(x) 
+                       { all_jsr %>% filter(varstr==x$label) %>% nrow }),
+                     label=map_chr(model_list,'label'))
+ggplot(plotme,aes(x=data,y=score,label=label)) +
+  geom_point(size=2,color='#BA0C2F') +
+  geom_text_repel() +
+  theme_USAID +
+  scale_x_log10() +
+  xlab('Number of data points') + ylab('NRMSE')
